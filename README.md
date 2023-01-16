c3238cf39a6f4c7f396333fe97948710690e0103
nkunic <45877400+nkunic@users.noreply.github.com>
2023-01-16 08:18:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-example-ngif-core-directive
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-example-ngfor-core-directive-nk-oypnsb)
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent {
+  @Input() course: Course;
+
+  @Input() cardIndex: number;
+
+  // @Output() customEventClicked = new EventEmitter<Course>();
+  @Output('customEventClicked') customEventEmitter = new EventEmitter<Course>();
+
+  onCustomButtonClicked() {
+    console.log('Card component - custom browser event clicked...');
+
+    // this.customEventClicked.emit(this.course);
+    this.customEventEmitter.emit(this.course);
+  }
+}
